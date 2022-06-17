808f3f362ca9ce3e1f597f8675e9ea1d03074433
RIP1982 <perovsky@list.ru>
2022-06-17 19:47:28 +0400

    Solution task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+
+Console.Clear();
+
+int number = Input("Input number: ");
+int sum = 0;
+
+while (number > 0)
+{
+    int div = number % 10;
+    sum = sum  + div;
+    number = number / 10;
+}
+
+Console.WriteLine($"Sum of digits = {sum}");
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
