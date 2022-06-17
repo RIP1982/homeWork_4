bab1e70172a8bbdedfc1bf343fd8c27eca7a77d2
RIP1982 <perovsky@list.ru>
2022-06-17 18:45:17 +0400

    Solution task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int A = Input("Input number A: ");
+int B = Input("Input number B: ");
+
+
+double square(double a, double b)
+{
+   return Math.Pow(a, b);
+}
+
+int Input(string output)
+{
+   Console.Write(output);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"number {A} to the power {B} = {square(A, B)}");
+
+
+
+
+
